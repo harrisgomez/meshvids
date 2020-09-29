14c977f7b9e8f19f2e902097d034bd8f49aab6ad
HARRIS GOMEZ <harrisg@MacBook-Pro.local>
2020-09-29 18:45:13 -0400

    Refactored video player code.

--- src/js/containers/VideoContainer.js
+++ src/js/containers/VideoContainer.js
@@ -3,41 +3,45 @@ import React, { Component } from 'react';
 
 export class VideoContainer extends Component {
     componentDidMount() {
-        (function localFileVideoPlayer() {
-          var URL = window.URL || window.webkitURL
-          var displayMessage = function (message, isError) {
-            var element = document.querySelector('#message')
-            element.innerHTML = message
-            element.className = isError ? 'error' : 'info'
-          }
-          var playSelectedFile = function (event) {
-            var file = this.files[0]
-            var type = file.type
-            var videoNode = document.querySelector('video')
-            var canPlay = videoNode.canPlayType(type)
-            if (canPlay === '') canPlay = 'no'
-            var message = 'Can play type "' + type + '": ' + canPlay
-            var isError = canPlay === 'no'
-            displayMessage(message, isError)
-        
-            if (isError) {
-              return
-            }
-        
-            var fileURL = URL.createObjectURL(file)
-            videoNode.src = fileURL
-          }
-          var inputNode = document.querySelector('input')
-          inputNode.addEventListener('change', playSelectedFile, false)
+        (() => {
+            const URL = window.URL || window.webkitURL;
+            const displayMessage = (message, isError) => {
+                const element = document.querySelector('#message');
+                element.innerHTML = message;
+                element.className = isError ? 'error' : 'info';
+            };
+            const playSelectedFile = function (event) {
+                const file = this.files[0];
+                const type = file.type;
+                const videoNode = document.querySelector('video');
+                let canPlay = videoNode.canPlayType(type);
+
+                if (canPlay === '') canPlay = 'no';
+
+                const message = 'Can play type "' + type + '": ' + canPlay;
+                const isError = canPlay === 'no';
+
+                displayMessage(message, isError);
+
+                if (isError) return;
+
+                const fileURL = URL.createObjectURL(file);
+
+                videoNode.src = fileURL;
+            };
+            const inputNode = document.querySelector('input');
+
+            inputNode.addEventListener('change', playSelectedFile, false);
         })()
     }
+
     render() {
         console.log(this.props.videoIds);
         return (
             <div id="video-container">
                 <div id="message"></div>
                 <input type="file" accept="video/*" />
-                <video controls autoplay></video>
+                <video controls autoPlay></video>
             </div>
         );
     }
