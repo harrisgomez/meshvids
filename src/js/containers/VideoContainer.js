9052d2f7f9004ed9b80971f3e01690943b39f5aa
HARRIS GOMEZ <harrisg@MacBook-Pro.local>
2020-09-29 19:51:56 -0400

    Removed comments.

--- src/js/containers/VideoContainer.js
+++ src/js/containers/VideoContainer.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react';
 import './VideoContainer.css';
-// import { Video } from '../components/Video';
+
+// TODO Detach upload button
+// TODO Create unordered list style of adding videos
 
 export class VideoContainer extends Component {
     componentDidMount() {
         (() => {
             const URL = window.URL || window.webkitURL;
-            // const displayMessage = (message, isError) => {
-            //     const element = document.querySelector('#message');
-            //     element.innerHTML = message;
-            //     element.className = isError ? 'error' : 'info';
-            // };
             const playSelectedFile = function (event) {
                 const file = this.files[0];
-                // const type = file.type;
                 const videoNode = document.querySelector('video');
-                // let canPlay = videoNode.canPlayType(type);
-
-                // if (canPlay === '') canPlay = 'no';
-
-                // const message = 'Can play type "' + type + '": ' + canPlay;
-                // const isError = canPlay === 'no';
-
-                // displayMessage(message, isError);
-
-                // if (isError) return;
-
                 const fileURL = URL.createObjectURL(file);
 
                 videoNode.src = fileURL;
@@ -38,8 +23,7 @@ export class VideoContainer extends Component {
 
     render() {
         return (
-            <div id="video-container" className=''>
-                {/* <div id="message"></div> */}
+            <div id="video-container">
                 <input id='video' style={{display: 'none'}} type="file" accept="video/*" />
                 <label htmlFor="video">Upload</label>
                 <video className='w-20' controls autoPlay></video>
